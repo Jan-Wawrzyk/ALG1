d190cbfcbceb85166446fb166809530f588340ac
Jan-Wawrzyk <honza.wawrzyk@seznam.cz>
2025-10-20 10:38:17 +0200

    Podminky - to co jsme delali v 1. hodině je zakomentované v Mainu.

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,73 @@
-import java.util.Scanner;
+import Podminky.Point;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        Scanner input = new Scanner(System.in);
+//
+//
+//        PC computer = new PC();
+//        computer.cpu = new CPU();
+//        computer.gpu = new GPU();
+//        computer.hdd = new HDD();
+//
+//        computer.cpu.power = 110;
+//        computer.cpu.consumption = 30;
+//        computer.cpu.unluckedMulitlier = 2;
+//
+//        computer.gpu.power = 300;
+//
+//        System.out.println(computer.cpu.toString());
+//        System.out.println(computer.cpu.power);
+//
+//        System.out.println(computer);
+        // ^^1. Hodina pak chtěl dělat podmínky ^^
 
+        Point p = new Point();
+        p.x = 2;
+        p.y = 1;
 
-        PC computer = new PC();
-        computer.cpu = new CPU();
-        computer.gpu = new GPU();
-        computer.hdd = new HDD();
+        if(p.x == p.y) {
+            System.out.println("uspech");
+        } else if (p.x != p.y) {
+            System.out.println("elsif");
+        } else if (p.x > p.y) {
+            System.out.println("elsif x > y");
+        } else {
+            System.out.println("nope");
+        }
+        // ternarni operator -> nevnořovat
 
-        computer.cpu.power = 110;
-        computer.cpu.consumption = 30;
-        computer.cpu.unluckedMulitlier = 2;
+        int greaterNumber = (p.x >= p.y) ?
+                p.x :
+                p.y;
 
-        computer.gpu.power = 300;
+        System.out.println(
+                p.y == p.x ?
+                "uspech" : "nope"
+        );
+        // switch
+        // totalni prasarna pouzivat jenom pro enumy
 
-        System.out.println(computer.cpu.toString());
-        System.out.println(computer.cpu.power);
+        switch (greaterNumber) {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+                // mužeš i nemusiš to wrapovat do {}
+            case 3: {
+                System.out.println("3");
+                break;
+            }
 
-        System.out.println(computer);
+            default: {
+                System.out.println("nope");
+            }
+        }
+
 
     }
 }
---
+++ src/Podminky/Point.java
@@ -0,0 +1,7 @@
+package Podminky;
+
+public class Point {
+    //public protoze to je v package normalne bys to neresila
+    public Integer x;
+    public Integer y;
+}
