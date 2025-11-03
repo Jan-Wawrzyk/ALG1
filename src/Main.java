8e32eab31a7085167555ec11390c1d5fdde324b3
Jan-Wawrzyk <honza.wawrzyk@seznam.cz>
2025-11-03 10:21:14 +0100

    Mdky co jsme delalli nebudu to rozdeovat na vic commitu

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,24 @@
-import Podminky.Point;
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-//
-//
-//        PC computer = new PC();
-//        computer.cpu = new CPU();
-//        computer.gpu = new GPU();
-//        computer.hdd = new HDD();
-//
-//        computer.cpu.power = 110;
-//        computer.cpu.consumption = 30;
-//        computer.cpu.unluckedMulitlier = 2;
-//
-//        computer.gpu.power = 300;
-//
-//        System.out.println(computer.cpu.toString());
-//        System.out.println(computer.cpu.power);
-//
-//        System.out.println(computer);
-        // ^^1. Hodina pak chtěl dělat podmínky ^^
-        // TEST
-        Point p = new Point();
-        p.x = 2;
-        p.y = 1;
 
-        if(p.x == p.y) {
-            System.out.println("uspech");
-        } else if (p.x != p.y) {
-            System.out.println("elsif");
-        } else if (p.x > p.y) {
-            System.out.println("elsif x > y");
-        } else {
-            System.out.println("nope");
-        }
-        // ternarni operator -> nevnořovat
+        // Chce všude ať používáme getry a setry
+        Person jakub = new Person(11);
 
-        int greaterNumber = (p.x >= p.y) ?
-                p.x :
-                p.y;
+        // tim že to je static nepotřebuje instanci
+        System.out.println(Person.ADULT_TRESHOLD);
 
-        System.out.println(
-                p.y == p.x ?
-                "uspech" : "nope"
-        );
-        // switch
-        // totalni prasarna pouzivat jenom pro enumy
+        //problemy s uživateli
+        Scanner sc = new Scanner(System.in);
 
-        switch (greaterNumber) {
-            case 1:
-                System.out.println("1");
-                break;
-            case 2:
-                System.out.println("2");
-                break;
-                // mužeš i nemusiš to wrapovat do {}
-            case 3: {
-                System.out.println("3");
-                break;
-            }
+        System.out.print("Enter your name: ");
+        jakub.setFirstName(sc.nextLine());
+        System.out.print(jakub.getFirstName());
 
-            default: {
-                System.out.println("nope");
-            }
-        }
+        System.out.print("Enter your age: ");
 
-
+        jakub.setAge(sc.nextInt());
+        System.out.println(jakub.getAge());
     }
 }
